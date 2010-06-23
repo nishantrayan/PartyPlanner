f56d77478977aaad0eb2c1ab52ea2324fb38cbc2
nishant rayan <nishantrayan@gmail.com>
2010-06-23 23:14:34 +0530

    View for adding task to a person

--- gen/com/rayan/partyplanner/R.java
+++ gen/com/rayan/partyplanner/R.java
@@ -10,14 +10,24 @@ package com.rayan.partyplanner;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int padding_top=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int add_button=0x7f060003;
+        public static final int person_name=0x7f060000;
+        public static final int task_date=0x7f060002;
+        public static final int task_title=0x7f060001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add_task_layout=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- src/com/rayan/partyplanner/AddTaskActivity.java
+++ src/com/rayan/partyplanner/AddTaskActivity.java
@@ -7,14 +7,12 @@ package com.rayan.partyplanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class AddTaskActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.add_task_layout);
     }
 }
