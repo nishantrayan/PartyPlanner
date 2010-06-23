34c197a9dd424637c50849bfafa5ef546b16e83d
nishant rayan <nishantrayan@gmail.com>
2010-06-23 22:27:10 +0530

    first commit

---
+++ src/com/rayan/partyplanner/MyActivity.java
@@ -0,0 +1,20 @@
+/**
+ * @Copyright (c) 2010 Amazon.com.  All rights reserved.
+ * <p/>
+ * @Version: $Id$
+ */
+package com.rayan.partyplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
